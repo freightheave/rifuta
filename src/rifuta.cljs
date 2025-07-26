3fc54c7b3c5bc9e065f9b13f37e80c0f8b30516f
Roshan <komaravolu.roshan@gmail.com>
2025-07-26 21:57:58 +0530

    26/07

--- src/rifuta.cljs
+++ src/rifuta.cljs
@@ -1,37 +1,54 @@
 (ns rifuta
-  (:require [replicant.dom :as r]))
+  (:require
+   [replicant.dom :as r]))
 
 (defonce store (atom nil))
 
-(defn render-logset-form [state] ;should return the HTML only. nope can handle everything here.
-  [:div.all
-   [:div.exercise "Exercise Name: " [:input {:type "text",
-                                             :on {:input (fn [e]
-                                                           (let [x (.-value (.-target e))]
-                                                             (swap! store (fn [state]
-                                                                            (assoc-in state [:current-set :exercise] x)))))}}]] ; add listeners to all the divs, :on :input.Replace test with value from last set of exercise. Also, look into "Placeholder" attribute for pre-fill.
-   [:div.weight "Weight: " [:input {:type "number",
-                                    :on {:input (fn [e]
-                                                  (let [x (.-valueAsNumber (.-target e))]
-                                                    (swap! store (fn [state]
-                                                                   (assoc-in state [:current-set :weight] x)))))}}]]
+(defn exercise-input [e]
+  (let [x (.-value (.-target e))]
+    (swap! store (fn [state]
+                   (assoc-in state [:current-set :exercise] x)))))
 
-   [:div.note "Note: " [:input {:type "text",
-                                :on {:input (fn [e]
-                                              (let [x (.-value (.-target e))]
-                                                (swap! store (fn [state]
-                                                               (assoc-in state [:current-set :note] x)))))}}]]
-   [:div.reps "Repetitions: " [:input {:type "number",
-                                       :on {:input (fn [e]
-                                                     (let [x (.-valueAsNumber (.-target e))]
-                                                       (swap! store (fn [state]
-                                                                      (assoc-in state [:current-set :reps] x)))))}}]]
+(defn weight-input [e]
+  (let [x (.-valueAsNumber (.-target e))]
+    (swap! store (fn [state]
+                   (assoc-in state [:current-set :weight] x)))))
+
+(defn note-input [e]
+  (let [x (.-value (.-target e))]
+    (swap! store (fn [state]
+                   (assoc-in state [:current-set :note] x)))))
+
+(defn reps-input [e]
+  (let [x (.-valueAsNumber (.-target e))]
+    (swap! store (fn [state]
+                   (assoc-in state [:current-set :reps] x)))))
+
+(defn store-set [] ; --submits to the atom, call button logic here.
+  (swap! store (fn [state]
+                 (let [curr (get state :current-set)
+                       all-sets (get state :all-sets)]
+                   (assoc state :all-sets (conj all-sets curr))))))
+
+(def handler-by-name {:exercise-input exercise-input
+                      :weight-input weight-input
+                      :note-input note-input
+                      :reps-input reps-input
+                      :store-set store-set})
+
+(defn render-logset-form [state] ; should return the HTML only. nope can handle everything here.
+  [:div.all
+   [:div.exercise "Exercise Name: " [:input {:type "text"
+                                             :on {:input [:exercise-input]}}]] ; add listeners to all the divs, :on :input.Replace test with value from last set of exercise. Also, look into "Placeholder" attribute for pre-fill.
+   [:div.weight "Weight: " [:input {:type "number"
+                                    :on {:input [:weight-input]}}]]
+
+   [:div.note "Note: " [:input {:type "text"
+                                :on {:input [:note-input]}}]]
+   [:div.reps "Repetitions: " [:input {:type "number"
+                                       :on {:input [:reps-input]}}]]
    [:div.submit [:button {:on
-                          {:click (fn [state] ;--submits to the atom, call button logic here.
-                                    (swap! store (fn [state]
-                                                   (let [curr (get state :current-set),
-                                                         all-sets (get state :all-sets)]
-                                                     (assoc state :all-sets (conj all-sets curr))))))}}
+                          {:click [:store-set]}}
                  "Submit"]]])
 
 (defn render-done-sets [state]
@@ -42,18 +59,16 @@
    (render-logset-form state)
    (render-done-sets state)])
 
-;Show the previous set. Can't render with the same function, so look into if it can be done with mulitple render functions or need to tweak the index.html to include more divs. why not? just stick another div in index.html and call another render func.
-(defn render-prevset-form [& args])
-
-(defn button-logic [& args]
-  ;something related to button goes here. (not really, can be handled in the button div fn only.)
-  )
-
 (defn main []
   (let [el (js/document.getElementById "app")]
+    (r/set-dispatch!
+      (fn [{e :replicant/dom-event :as event-data} [function-name & args :as handler-data]]
+        (when (= :replicant.trigger/dom-event (:replicant/trigger event-data))
+          (apply (handler-by-name function-name)
+                 e args))))
     (add-watch store :watcher (fn [_ _ _ state]
                                 (r/render el (render-app state))))
-    (reset! store {:current-set {}, :all-sets []}))) ;always 'CALL' the render function.
+    (reset! store {:current-set {}, :all-sets []}))) ; always 'CALL' the render function.
 
 ; --- Testing code from here
 
--- src/test.cljs
+++ src/test.cljs
@@ -1,22 +1,38 @@
 (ns test
-  (:require [replicant.dom :as r]))
+  (:require
+   [replicant.dom :as r]))
 
 (def a (atom nil))
 
+(defn button-click-fn [& args]
+  (swap! a (fn [m]
+             (assoc m :incremented
+                    (+ (:incremented m) (:incrementor m))))))
+
+(defn input-box-fn [e]
+  (let [x (.-valueAsNumber (.-target e))]
+    (js/console.log "global dispatch handler let for input box")
+    (swap! a (fn [m]
+               (assoc m :incrementor x)))))
+
+(r/set-dispatch!
+  (fn [event-data handler-data]
+    (when (= :replicant.trigger/dom-event (:replicant/trigger event-data))
+      (let [e (:replicant/dom-event event-data)
+            m {:button-click-fn button-click-fn
+               :input-box-fn input-box-fn}]
+        (apply (m (first handler-data))
+               e (rest handler-data))))
+    (js/console.log event-data handler-data)))
+
 (defn render-inc-test [m]
   [:div#counter
-   [:input#inputbox {:type "number",
-                     :value (:incrementor m),
-                     :on {:input (fn [e]
-                                   (let [x (.-valueAsNumber (.-target e))]
-                                     (swap! a (fn [m]
-                                                (assoc m :incrementor x)))))}}] ;(swap! a fn [m] (assoc m :incrementor x)) == (apply f '(current value of a) '(no args here)) -> this means that f is being called with current value of a (which is the map.)
+   [:input#inputbox {:type "number"
+                     :value (:incrementor m)
+                     :on {:input [:input-box-fn]}}] ; (swap! a fn [m] (assoc m :incrementor x)) == (apply f '(current value of a) '(no args here)) -> this means that f is being called with current value of a (which is the map.)
+
    [:div#displaythething (:incremented m)]
-   [:button {:on {:click
-                  (fn [e]
-                    (swap! a (fn [m]
-                               (assoc m :incremented
-                                      (+ (:incremented m) (:incrementor m))))))}}
+   [:button {:on {:click [:button-click-fn 1]}}
 
     "Click Me"]])
 
@@ -26,5 +42,17 @@
                             (r/render el (render-inc-test new-state))))
     (reset! a {:incrementor 1, :incremented 0})))
 
+;;; TEST
 
+(comment
+  (def handler-data "lol")
+  (case (first handler-data)
+    (\l) :test1
+    (\o) :test2)
+  (if false
+    (prn "lkmao")
+    (if true
+      (prn)
+      (prn "test")))
 
+  ,)
