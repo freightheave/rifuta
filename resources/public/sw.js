adf16d107536ae3d40debe1e62f80b953cf73607
Roshan <komaravolu.roshan@gmail.com>
2025-07-31 20:07:48 +0530

    release

---
+++ resources/public/sw.js
@@ -0,0 +1,16 @@
+// Establish a cache name
+const cacheName = 'rifuta-cache';
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        const fetchedResponse = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+
+          return networkResponse;
+        });
+
+        return cachedResponse || fetchedResponse;
+      });
+    }));
+});
