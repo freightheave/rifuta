7dca047ed9945ba2c9ab66956ef0511ea7f49006
Roshan <komaravolu.roshan@gmail.com>
2025-08-02 15:52:42 +0530

    sergice worker shenanigans

--- resources/release/sw.js
+++ resources/release/sw.js
@@ -1,6 +1,22 @@
 // Establish a cache name
 const cacheName = 'rifuta-cache';
 
+const appShellFiles = [
+  './',
+  './index.html',
+  './styles.css',
+  './app-js/main.js',
+  './app-js/manifest.edn'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(caches.open(cacheName).then((cache) => {
       return cache.match(event.request).then((cachedResponse) => {
